58e5a1d5a70998f9b4ead15a774eb142e2f1c325
hdelalande <hugo.delalande@insa-lyon.fr>
2020-12-08 23:39:56 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,57 +8,77 @@ import (
   "log"
 )
 
-type Pixel struct{
-  r uint32
-  g uint32
-  b uint32
-  a uint32
+type pixel struct{ /*Notre structure d'un pixel = son intensité, son opacité, sa position en x et sa position en y*/
+  intensPix uint32
+  opac uint32
+  Dx int
+  Dy int 
 }
 
 func main() {
   argument := os.Args[1]
   catFile, err := os.Open(argument)
   if err != nil {
-	  log.Fatal(err) // ça renvoie une erreur si image introuvable
+	  log.Fatal(err) /* Fatal erreur si il n'y a pas de fichier à ouvrir en argument */ 
   }
   defer catFile.Close()
 
   imData , err := jpeg.Decode(catFile)
   if err != nil {
-        fmt.Println(err)
+        fmt.Println(err) /* Renvoi une erreur si le fichier n'est pas une image jpeg ou jpg */
   }
+
+  var list []pixel
+  var testNoirEtBlanc bool
+  var tabIntens [2^32-1]uint32
+  taille := imData.Bounds()
+	hauteur := taille.Dy()
+  largeur := taille.Dx() 
+  /* 
+  ici il faudra décomposer la hauteur et la largeur dans d'autres
+  variables pour faire  fonctionner les goroutines 
+  */
+  testNoirEtBlanc = true
+  for i:=0; i<largeur; i++ {
+    for j:=0; j<hauteur; j++{
+      r,g,b,a := imData.At(i,j).RGBA()
+      if r!=g || g!=b || r!=b { /*test si les intensités RGB sont différente pour detecter si l'image est en couleur*/
+        testNoirEtBlanc = false 
+        fmt.Println("Tu dois nous envoyer une image en noir et blanc bg")
+        break
+      } 
+      InfoPixel := pixel{intensPix:r, Dx:i, Dy:j, opac:a} 
+      list = append(list, InfoPixel) /*Stockage des infos de chaque pixels dans une liste*/
+      tabIntens [r] += 1 /* Génére un tableau ayant comme indice l'insité du pixel et en valeur le nombre total de pixels ayant cette intensité*/
+    }
+  }
+  if testNoirEtBlanc == false {
+    /*Dans cette partie on peut imaginer un code permettant de faire passer une image en N&B avant de faire le traitement de l'image*/
+  }
+  if testNoirEtBlanc == true {
+    /*histogramme, égalisation et normalisation des pixels*/
+  }
+    fmt.Println(list[9].intensPix)
+    fmt.Println(testNoirEtBlanc)
+}
+
+
+
+  /*
+    fmt.Println(hauteur)
+    fmt.Println(largeur)
     fmt.Println(imData.At(5,5))
     fmt.Println(imData.Bounds())
 
-  c := imData.At(5,5)
-  r,g,b,a := c.RGBA()
-  fmt.Printf("c:%v,r:%v\n",c,r/255)
-  
-
-  fmt.Println(r/256,g,b,a)
-  valeur := uint8(int(r))
-  fmt.Println(valeur)
-  fmt.Println(r)
-
-  /*taille := imData.Bounds()
-	hauteur := taille.Dy()
-	largeur := taille.Dx()
-  fmt.Println(hauteur)
-  fmt.Println(largeur)
-  
-  for i:=0; i<largeur; i++ {
-		for j:=0; j<hauteur; j++{
-			 imData.At(i,j).RGBA())
-		}
-	}*/
-
-   /* var ValeurPixel [256]int
+    var ValeurPixel [256]int
     ValeurPixel[0] = imData.At(0,0)
-    fmt.Println(ValeurPixel[0])*/
+    fmt.Println(ValeurPixel[0])
 
-    //cat, err := jpeg.Decode(catFile)
-    //if err != nil {
-      //  log.Fatal(err)
-    //}
-    //fmt.Println(cat)
-}
+    cat, err := jpeg.Decode(catFile)
+    if err != nil {
+      log.Fatal(err)
+    }
+    fmt.Println(cat)
+    */
+
+  
